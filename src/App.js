797ceee6f6ba19b7ebe6d9240932b171b79c4434
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-26 04:00:52 +0100

    back to building feedback

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const commemts = [
-    {id: 1, text: 'Comment one'},
-    {id: 2, text: 'Comment two'},
-    {id: 3, text: 'Comment three'}
-  ]
-
-  const loading = false
-  const showComments = true
-  const commentBlock = (
-    <div className="comments">
-    <h3>Commemts ({commemts.length})</h3>
-    <ul>
-      {commemts.map((comment, index) => (<li key={index}>{comment.text}</li>))}
-    </ul>
-  </div>
-  )
-
   if(loading) return <h1>Loading...</h1>
   return (
     <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments && commentBlock}
+      <h1>My App</h1>
     </div>
   );
 }
