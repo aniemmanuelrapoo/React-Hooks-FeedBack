2a3014206f6ca9f2e2d4a75a969f41a72a0a1ae5
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-12-26 03:58:58 +0100

    Conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ function App() {
     {id: 2, text: 'Comment two'},
     {id: 3, text: 'Comment three'}
   ]
+
+  const loading = false
+  const showComments = true
+  const commentBlock = (
+    <div className="comments">
+    <h3>Commemts ({commemts.length})</h3>
+    <ul>
+      {commemts.map((comment, index) => (<li key={index}>{comment.text}</li>))}
+    </ul>
+  </div>
+  )
+
+  if(loading) return <h1>Loading...</h1>
   return (
     <div className="container">
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
-      <div className="comments">
-        <h3>Commemts ({commemts.length})</h3>
-        <ul>
-          {commemts.map((comment, index) => (<li key={index}>{comment.text}</li>))}
-        </ul>
-      </div>
+      {showComments && commentBlock}
     </div>
   );
 }
